5149289fba8aa0a5d790f5a443b0b866656929f6
Stephen Boettcher <sboettcher@pch.com>
2025-02-09 08:20:39 -0500

    add a footer and added something to show loading when loads slower

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import './app.css'
 import './printer.css'
 import App from './App.svelte'
 
+const target: HTMLElement | null = document.getElementById('app');
+// clear temp children
+if (target) {
+  target.innerHTML = '';
+}
+
 const app = new App({
-  target: document.getElementById('app')!,
+  target: target!,
 })
 
 export default app
