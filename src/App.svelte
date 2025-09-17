1253b6e0cb0a8a110144f91ff346c229aab55f49
Stephen Boettcher <sboettcher@macbookpro.myfiosgateway.com>
2025-09-17 16:21:31 -0400

    add contact form

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
   import { Router, Route, navigate } from "svelte-routing";
-  import { setContext } from 'svelte';
+  import { setContext } from "svelte";
   import Home from "./pages/Home.svelte";
   import Portfolio from "./pages/Portfolio.svelte";
   import Resume from "./pages/Resume.svelte";
   import Blog from "./pages/Blog.svelte";
   import Career from "./pages/Career.svelte";
+  import Contact from "./pages/Contact.svelte";
   import Navigation from "./components/Navigation.svelte";
 
   export let url = "";
 
   const urlParams = new URLSearchParams(window.location.search);
-  const resType = urlParams.get('type') || "fe_dev";
-  const page = urlParams.get('page') || "";
-  const post = urlParams.get('post') || "";
-
+  const resType = urlParams.get("type") || "fe_dev";
+  const page = urlParams.get("page") || "";
+  const post = urlParams.get("post") || "";
 
   setContext("resumeType", resType);
 
@@ -22,58 +22,59 @@
     fe_dev: "resume_dev.json",
     full_stack: "resume_full.json",
     tpm: "resume_tpm.json",
-    ba: "business_analyst.json"
+    ba: "business_analyst.json",
   };
 
   setContext("resumeFiles", resumeFiles);
 
   const links = [
     {
-      "path": '/',
-      "component": Home,
-      "name": "Home"
-    },    
-    {
-      "path": '/resume',
-      "component": Resume,
-      "name": "Resume"
+      path: "/",
+      component: Home,
+      name: "Home",
     },
     {
-      "path": '/career',
-      "component": Career,
-      "name": "Career"
+      path: "/resume",
+      component: Resume,
+      name: "Resume",
     },
     {
-      "path": '/portfolio',
-      "component": Portfolio,
-      "name": "Portfolio"
+      path: "/career",
+      component: Career,
+      name: "Career",
     },
     {
-      "path": '/blog',
-      "component": Blog,
-      "name": "Blog"
-    }
+      path: "/contact",
+      component: Contact,
+      name: "Contact",
+    },
+    {
+      path: "/portfolio",
+      component: Portfolio,
+      name: "Portfolio",
+    },
+    {
+      path: "/blog",
+      component: Blog,
+      name: "Blog",
+    },
   ];
 
-  const navExcludeList: string[] = [
+  const navExcludeList: string[] = [];
 
-  ];
-
-  console.log("parse url:", window.location.pathname.split('/'));
+  console.log("parse url:", window.location.pathname.split("/"));
 
   console.log(`\\${page}`);
-  if(page) navigate(post ? `\\${page}?post=${post}` : `\\${page}`);
-
-
+  if (page) navigate(post ? `\\${page}?post=${post}` : `\\${page}`);
 </script>
 
 <Router {url}>
-  <Navigation links={links} exclude={navExcludeList}/>
+  <Navigation {links} exclude={navExcludeList} />
   <main class="main-content">
     {#each links as link}
-    <Route path="{link.path}">
-      <svelte:component this={link.component} />
-    </Route>
+      <Route path={link.path}>
+        <svelte:component this={link.component} />
+      </Route>
     {/each}
   </main>
 </Router>
---
+++ src/pages/Contact.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Contact from '../components/Contact.svelte';
+</script>
+
+<svelte:head>
+	<title>Stephen Boettcher - Contact</title>
+    <link rel="canonical" href="https://sboettcher.com/contact">
+    <meta name="description" content="Stephen Boettcher's career Contact page">
+</svelte:head>
+
+<div class="career-wrapper page-content">
+    <Contact />
+</div>
